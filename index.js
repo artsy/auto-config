b941b1f47a6cab9dba0b1d25fcbf50863b12ec83
Justin Bennett <zephraph@gmail.com>
2019-01-22 11:57:05 -0500

    Add initial artsy config

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  noVersionPrefix: true,
+  name: "Artsy",
+  email: "it@artsymail.com",
+  labels: {
+    major: "Version: Major",
+    minor: "Version: Minor",
+    patch: "Version: Patch",
+    internal: "Version: Trivial"
+  },
+  skipReleaseLabels: ["Version: Trivial", "Skip Release"]
+}
+
